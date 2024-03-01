80456537d1189184ebce5f75f354485aa5051ef9
gifloic <loic.martin@cea.fr>
2024-03-01 23:06:46 +0100

    Clean code + commentaire + NE NLTK

--- Projet_TAL/Sujet_1/sujet1.py
+++ Projet_TAL/Sujet_1/sujet1.py
@@ -1,141 +1,362 @@
 import nltk
 import sys
+import os
 # nltk.download('punkt')
 # nltk.download('averaged_perceptron_tagger')
+# nltk.download('maxent_ne_chunker')
+# nltk.download('words')
 from nltk.tokenize import word_tokenize
 from nltk          import RegexpParser
+from nltk.tag import StanfordPOSTagger
 
-import aymara.lima
-nlp = aymara.lima.Lima("ud-eng")
+java_path = "C:/Program Files/Java/jdk-18.0.1.1/bin/java.exe"
+os.environ['JAVAHOME'] = java_path
+
+stanford_pos_jar   = 'stanford-postagger.jar'
+stanford_pos_model = 'english-bidirectional-distsim.tagger'
+st = StanfordPOSTagger(stanford_pos_model, stanford_pos_jar)
+
 
 
 
 
 ########## VARIABLES GLOBALES ##########
-SENTENCES = []
-REF_PTB   = {}
-PTB_UNIV  = {}
-TAG_LIMA  = []
+SENTENCES_TAG = []
+SENTENCES_NE  = []
+TAG_STANFORD  = []
+REF_PTB       = {}
+PTB_UNIV      = {"HYPH":".", "ADD":".", "NFP":"NOUN", "-LRB-":".","-RLB-":"."}
+C0NLL2003     = { "PERSON": "PER",
+                  "ORGANIZATION": "ORG",
+                  "LOCATION": "LOC" }
 
-TEXT_REF_FILE     = "pos_reference.txt"
-TEXT_RES_FILE     = "pos_test.txt"
+########## FICHIERS ANALYSE MORPHO-SYNTAXIQUE ##########
+TEXT_REF_FILE_TAG = "pos_reference.txt"
+TEXT_RES_FILE_TAG = "pos_test.txt"
 POSTAGS_REF_PTB   = "POSTags_REF_PTB.txt" 
 POSTAGS_PTB_UNIV  = "POSTags_PTB_Universal.txt"
 POSTAGS_UNIV_FILE = "pos_reference.txt.univ"
 POSTAG_NLTK_FILE  = "pos_test.txt.pos.nltk"
-POSTAG_LIMA_FILE  = "pos_test.txt.pos.lima"
+POSTAG_STANFORD_FILE = "pos_test.txt.pos.stanford"
 POSTAG_UNIV_NLTK_FILE = "pos_test.txt.pos.nltk.univ" 
-POSTAG_UNIV_LIMA_FILE = "pos_test.txt.pos.lima.univ"
+POSTAG_UNIV_STANFORD_FILE = "pos_test.txt.pos.stanford.univ"
+
+########## FICHIERS RECONNAISSANCE ENTITES NOMMEES ##########
+TEXT_REF_FILE_NE   = "ne_reference.txt.conll"
+TEXT_RES_FILE_NE   = "ne_test.txt"
+NE_NLTK_FILE       = "ne_test.txt.ne.nltk"
+NE_CONLL_NLTK_FILE = "ne_test.txt.ne.nltk.conll"
+
+
+
+
 
 ########## FONCTIONS GLOBALES ##########
-def extract_postags_ref_ptb_files(ref_ptb_file):
-    try:
-        with open(ref_ptb_file, 'r') as file:
-            for line in file:
-                l = line.split()
-                REF_PTB[l[0]] = l[1]
-    except FileNotFoundError:
-        print(f"File \"{ref_ptb_file}\" not found.")
-
-def extract_postags_ptb_univ_files(ptb_univ_file):
-    try:
-        with open(ptb_univ_file, 'r') as file:
-            for line in file:
-                l = line.split()
-                PTB_UNIV[l[0]] = l[1]
-    except FileNotFoundError:
-        print(f"File \"{ptb_univ_file}\" not found.")
-
-def create_dic_ref_universal(ref_ptb_file, ptb_univ_file):
-    extract_postags_ref_ptb_files(ref_ptb_file)
-    extract_postags_ptb_univ_files(ptb_univ_file)
-
-def extract_sentences_file(text_ref_file):
-    sentence = ""
-    try:
-        with open(text_ref_file, 'r') as file:
-            for line in file:
-                if len(line) == 1:
-                    continue
-                else:
-                    l = line.split("\t")
-                    if l[0] == ".":
-                        sentence = sentence[:-1] + l[0]
-                        SENTENCES.append(sentence)
-                        sentence = ""
-                    elif l[0] == ",":
-                        sentence = sentence[:-1] + l[0] + " "
-                    else:
-                        sentence += l[0] + " "
-    except FileNotFoundError:
-        print(f"File \"{text_ref_file}\" not found.")
-
 def open_file(text_file):
+    """
+    Args:
+        text_file (str): Chemin du fichier texte à ouvrir.
+
+    Returns:
+        str or None: Contenu du fichier texte si le fichier est trouvé et lu avec succès,
+                     sinon retourne None.
+    """
+    # Tente d'ouvrir le fichier spécifié en mode lecture ('r') sinon retourne une erreur
     try:
         with open(text_file, 'r') as file:
             text = file.read()
         print(f"Text file \"{text_file}\" loaded.")
+        return text
     except FileNotFoundError:
         print(f"File \"{text_file}\" not found.")
-    return text
+        return None
 
-def write_sentences_file(text_res_file):
-    with open(text_res_file, 'w') as file:
-        for sentence in SENTENCES:
+def extract_sentences_file(file, sentences):
+    """
+    Args:
+        file      (str)  : Le chemin vers le fichier contenant les tokens.
+        sentences (array): Le tableau contenant les phrases crées à partir du fichier.
+
+    Returns:
+        None: La fonction ne retourne rien, elle remplit le tableau avec des phrases à partir du fichier contenant les tokens.
+    """
+    
+    s = ""
+    
+    # Récupère le contenu du fichier et s'il n'est pas None, on procède ligne par ligne (split("\n"))
+    text = open_file(file)
+    if text is not None:
+        for line in text.split("\n"):
+            # Divise la ligne avec le séparateur tabulation
+            l = line.split("\t")
+            
+            if file == TEXT_REF_FILE_TAG:
+                # Si le fichier passé en paramètre est TEXT_REF_FILE_TAG, le changement de ligne se produit quand l a une taille de 1
+                if len(l) == 1:
+                    sentences.append(s)
+                    s = ""
+                # Si nous ne sommes pas au changement de ligne, nous remplissons la variable sentence avec un format différent selon ce que contient l[0]
+                else:
+                    if l[0] == ".":
+                        s = s[:-1] + l[0]
+                    elif l[0] == ",":
+                        s = s[:-1] + l[0] + " "
+                    else:
+                        s += l[0] + " "            
+            else:
+                # Si le fichier passé en paramètre est différent, le changement de ligne se produit quand l[0] est égal à ""
+                if l[0] == "":
+                    sentences.append(s)
+                    s = ""
+                # Si nous ne sommes pas au changement de ligne, nous remplissons la variable sentence avec un format différent selon ce que contient l[0]
+                else:
+                    if l[0] == ".":
+                        s = s[:-1] + l[0]
+                    elif l[0] == ",":
+                        s = s[:-1] + l[0] + " "
+                    else:
+                        s += l[0] + " "             
+
+def extract_tag_stanford_conll(text_res_file):
+    try:
+        with open(text_res_file, 'r') as file:
+            index = 0
+            for line in file:
+                if index == 10:
+                    break
+                sentence = st.tag(line.split())
+                for s in sentence:
+                    TAG_STANFORD.append((s[0], s[1]))
+                index += 1                
+    except FileNotFoundError:
+        print(f"File \"{text_res_file}\" not found.")
+
+def extract_postags_ref_ptb_files(file, type):
+    """
+    Args:
+        file (str): Le chemin vers le fichier de référence.
+        type (str): Le type de la table de correspondance des POS tags.
+
+    Returns:
+        None: La fonction ne retourne rien, elle remplit simplement un dictionnaire soit REF_PTB soit PTB_UNIV.
+
+    Remarque:
+        Le format du fichier de référence doit être sous la forme de lignes où la première colonne
+        est une étiquette et la deuxième colonne est son étiquette équivalente dans une autre convention.
+    """
+    # Récupère le contenu du fichier et s'il n'est pas None, on procède ligne par ligne (split("\n"))
+    text = open_file(file)
+    if text is not None:
+        for line in text.split("\n"):
+            # Divise la ligne avec le séparateur espace
+            l = line.split()
+            # Vérifie le nom du fichier fourni en argument
+            if type == "REF_PTB":
+                # Si le fichier est POSTAGS_REF_PTB, stocke les informations dans le dictionnaire REF_PTB
+                REF_PTB[l[0]] = l[1]
+            else:
+                # Sinon, stocke les informations dans le dictionnaire PTB_UNIV
+                PTB_UNIV[l[0]] = l[1]
+
+def create_dic_ref_universal(ref_ptb_file, ptb_univ_file):
+    """
+    Args:
+        ref_ptb_file  (str): Le chemin vers le fichier des étiquettes PTB de référence.
+        ptb_univ_file (str): Le chemin vers le fichier UNIV de référence.
+
+    Returns:
+        None: La fonction ne retourne rien, elle appelle "extract_postags_ref_ptb_files" pour créer les dictionnaire globaux REF_PTB et PTB_UNIV.
+    """
+    extract_postags_ref_ptb_files(ref_ptb_file,  "REF_PTB")
+    extract_postags_ref_ptb_files(ptb_univ_file, "PTB_UNIV")
+    print("Create dictionnary REF/PTB and PTB/UNIV.")
+                        
+                        
+
+def write_sentences_file(text, sentences):
+    """
+    Args:
+        text      (str)  : Le chemin du fichier où l'on veut écrire les phrases.
+        sentences (array): Le tableau contenant les phrases qui ont été construite grâce à la fonction "extract_sentences_file".
+
+    Returns:
+        None: La fonction ne retourne rien, elle remplit le fichier passé par référence à partir des phrases contenant dans le tableau.
+    """
+    with open(text, 'w') as file:
+        # Ecriture de chaque phrase (exceptée la dernière) contenu dans le tableau + saut de ligne
+        for sentence in sentences[:-1]:
             file.write(sentence + "\n")
-    print(f"Constructing sentences for text : File \"{text_res_file}\" created.")
+        # Ecriture de la dernière phrase du tableau sans ajout de saut de ligne
+        file.write(sentences[-1])
+    print(f"Constructing sentences for text : File \"{text}\" created.")
 
 def write_postags_universal_file(postags_reference_file, postags_universal_file, tag):
+    """
+    Args:
+        postags_reference_file (str): Le chemin du fichier où sont contenus les tokens de référence.
+        postags_universal_file (str): Le chemin du fichier où l'on veut écrire les tokens universels.
+        tag (str): Type du tag que l'on veut associé au token
+
+    Returns:
+        None: La fonction ne retourne rien, elle remplit le fichier output en remplaçant les tokens du fichier input par les tokens universels.
+        
+    Remarque:
+        Le format du fichier de sortie est un token de référence suivi d'une tabulation et du token universel associé à ce mot.
+    """
     try:
         with open(postags_reference_file, 'r') as input_file, open(postags_universal_file, 'w') as output_file:
             for line in input_file:
+                # Si la ligne à une longueur de 1 (équivalent à vide), on saute de ligne
                 if len(line) == 1:
                     output_file.write("\n")
                 else:
+                    # Divise la ligne avec le séparateur tabulation
                     l = line.split("\t")
+                    
+                    # Ecriture du premier token de la ligne suivi d'une tabulation suivi du token universel (récupéré grâce à différents dictionnaires selon le tag)
                     if tag == "REF":
                         output_file.write(l[0] + "\t" + PTB_UNIV[REF_PTB[l[1][:-1]]] + "\n")
                     elif tag == "PTB":
                         output_file.write(l[0] + "\t" + PTB_UNIV[l[1][:-1]] + "\n")
+            print(f"Universal Postags : File \"{postags_universal_file}\" created.")
     except FileNotFoundError:
         print(f"File \"{postags_reference_file}\" not found.")
-    print(f"Universal Postags : File \"{postags_universal_file}\" created.")
 
-def extract_tag_lima_conll(text_res_file):
-    try:
-        with open(text_res_file, 'r') as file:
-            for line in file:
-                print(file)
-                sentence = nlp(line)
-                for s in sentence:
-                    for token in s:
-                        TAG_LIMA.append((token.form, token.upos))
-    except FileNotFoundError:
-        print(f"File \"{text_res_file}\" not found.")
+def write_tag_file(token_tag_file, text, postagger):
+    """
+    Args:
+        token_tag_file (str): Le chemin du fichier où l'on veut écrire les tokens.
+        text (array): Tableau contenant les phrases à tokeniser.
+        postagger (str): Nom du postager.
 
-
-def write_tag_file(token_tag_file, tag, postagger):
+    Returns:
+        None: La fonction ne retourne rien, elle remplit le fichier des tokens des phrases contenues dans le tableau passé en paramètre.
+        
+    Remarque:
+        Le format du fichier de sortie est un mot suivi d'une tabulation et du token associé à ce mot. 
+        Une ligne vide signifie le changement de phrase.
+    """
     with open(token_tag_file, 'w') as file:
-        for token in tag:
-            file.write(token[0] + "\t" + token[1] + '\n')
-            if token[0] == ".":
-                file.write("\n")
+        for line in text:
+            # On tokenise chaque ligne du texte grâce au postagger de NLTK
+            tokens_TAG   = word_tokenize(line)
+            tag_nltk_TAG = nltk.pos_tag(tokens_TAG)
+            
+            # On écrit sur une ligne chaque mot du texte suivi d'une tabulation suivi du token NLTK
+            for token in tag_nltk_TAG:
+                file.write(token[0] + "\t" + token[1] + '\n')
+                
+            # On ajoute une ligne vide pour signifier la fin d'une phrase
+            file.write("\n")
     print(f"Tokenisation {postagger} achieved : File \"{token_tag_file}\" created.")
+    
+def write_named_entities_file(named_entities_file, text, postagger):
+    """
+    Args:
+        named_entities_file (str): Le chemin du fichier où l'on veut écrire les entités nommées.
+        text (array): Tableau contenant les phrases à tokeniser.
+        postagger (str): Nom du postager.
+
+    Returns:
+        None: La fonction ne retourne rien, elle remplit le fichier des entités nommées des phrases contenues dans le tableau passé en paramètre.
+        
+    Remarque:
+        Le format du fichier de sortie est un mot suivi d'une tabulation et de l'entité nommée associé à ce mot. 
+        Une ligne vide signifie le changement de phrase.
+    """
+    with open(named_entities_file, 'w') as file:
+        for line in text:
+            # On tokenise avec les entités nommées chaque ligne du texte grâce au chunk de NLTK
+            tokens_NE   = word_tokenize(line)
+            tag_nltk_NE = nltk.pos_tag(tokens_NE)
+            entities = nltk.ne_chunk(tag_nltk_NE, binary=False)
+            
+            # On écrit sur une ligne chaque mot du texte suivi d'une tabulation suivi de l'entité nommée NLTK
+            for token in entities:
+                # Si token[0] est un string alors ce mot n'est pas une entité nommée donc on on l'associe à l'entité nommée "O"
+                if isinstance(token[0], str):
+                    file.write(token[0] + "\t" + "O" + '\n')
+                # Sinon, nous avons une entité nommée et associons chaque mot de l'entité nommée au label de celle-ci
+                else:
+                    for item in token:
+                        file.write(item[0] + "\t" + token.label() + '\n')
+                        
+            # On ajoute une ligne vide pour signifier la fin d'une phrase
+            file.write("\n")
+    print(f"Tokenisation {postagger} achieved : File \"{named_entities_file}\" created.")
+    
+def write_named_entities_conll2003_file(named_entities_file, text, postagger):
+    """
+    Args:
+        named_entities_file (str): Le chemin du fichier où l'on veut écrire les entités nommées converties en étiquette CoNLL-2003.
+        text (array): Tableau contenant les phrases à tokeniser.
+        postagger (str): Nom du postager.
+
+    Returns:
+        None: La fonction ne retourne rien, elle remplit le fichier des entités nommées (format CoNLL-2003) des phrases contenues dans le tableau passé en paramètre.
+        
+    Remarque:
+        Le format du fichier de sortie est un mot suivi d'une tabulation et de l'entité nommée (format CoNLL-2003) associé à ce mot. 
+        Une ligne vide signifie le changement de phrase.
+    """
+    with open(named_entities_file, 'w') as file:
+        for line in text:
+            # On tokenise avec les entités nommées chaque ligne du texte grâce au chunk de NLTK
+            tokens_NE   = word_tokenize(line)
+            tag_nltk_NE = nltk.pos_tag(tokens_NE)
+            entities = nltk.ne_chunk(tag_nltk_NE, binary=False)
+            
+            # On écrit sur une ligne chaque mot du texte suivi d'une tabulation suivi de l'entité nommée NLTK convertie au format CoNLL-2003
+            for token in entities:
+                # Si token[0] est un string alors ce mot n'est pas une entité nommée donc on on l'associe à l'entité nommée "O"
+                if isinstance(token[0], str):
+                    file.write(token[0] + "\t" + "O" + '\n')
+                # Sinon, nous avons une entité nommée et associons chaque mot de l'entité nommée au label de celle-ci convertie au format CoNLL-2003
+                else:
+                    # Si le label est présent dans le dictionnaire CONNL2023, alors on l'associe au mot sinon on associe "MISC"
+                    if token.label() in C0NLL2003:
+                        ne = "-" + C0NLL2003[token.label()]
+                    else:
+                        ne = "-MISC"
+                        
+                    # On ajoute un spécificateur à l'entité nommée avec un B si c'est le premier token de celle-ci et un I sinon
+                    for index, item in enumerate(token):
+                        if index == 0:
+                            ne_token = "B" + ne
+                        else:
+                            ne_token = "I" + ne
+                        file.write(item[0] + "\t" + ne_token + '\n')
+            
+            # On ajoute une ligne vide pour signifier la fin d'une phrase
+            file.write("\n")
+    print(f"Tokenisation {postagger} achieved : File \"{named_entities_file}\" created.")
 
 
 ##### MAIN #####
-extract_sentences_file(TEXT_REF_FILE)
-write_sentences_file(TEXT_RES_FILE)
+
+# CREATION DICTIONNAIRE ETIQUETTE
 create_dic_ref_universal(POSTAGS_REF_PTB, POSTAGS_PTB_UNIV)
-write_postags_universal_file(TEXT_REF_FILE, POSTAGS_UNIV_FILE, "REF")
+write_postags_universal_file(TEXT_REF_FILE_TAG, POSTAGS_UNIV_FILE, "REF")
 
-text   = open_file(TEXT_RES_FILE)
-tokens = word_tokenize(text)
-# extract_tag_lima_conll(TEXT_RES_FILE)
-tag_nltk = nltk.pos_tag(tokens)
+# EXTRACTION ET ECRITURE DES PHRASES A PARTIR DES TOKENS MORPHO-SYNTAXIQUE
+extract_sentences_file(TEXT_REF_FILE_TAG, SENTENCES_TAG)
+write_sentences_file(TEXT_RES_FILE_TAG, SENTENCES_TAG)
 
-write_tag_file(POSTAG_NLTK_FILE, tag_nltk, "NLTK")
+# EXTRACTION ET ECRITURE DES PHRASES A PARTIR DES TOKENS ENTITES NOMMEES
+extract_sentences_file(TEXT_REF_FILE_NE, SENTENCES_NE)
+write_sentences_file(TEXT_RES_FILE_NE, SENTENCES_NE)
+
+# ANALYSE MORPHO-SYNTAXIQUE
+text_TAG = open_file(TEXT_RES_FILE_TAG)
+write_tag_file(POSTAG_NLTK_FILE, text_TAG.split("\n"), "NLTK")
 write_postags_universal_file(POSTAG_NLTK_FILE, POSTAG_UNIV_NLTK_FILE, "PTB")
 
-print(TAG_LIMA)
-# write_tag_file(POSTAG_LIMA_FILE, TAG_LIMA, "LIMA")
+extract_tag_stanford_conll(TEXT_RES_FILE_TAG)
+write_tag_file(POSTAG_STANFORD_FILE, TAG_STANFORD, "STANFORD")
+write_postags_universal_file(POSTAG_STANFORD_FILE, POSTAG_UNIV_STANFORD_FILE, "PTB")
+
+# EXTRACTION ENTITES NOMMEES
+text_NE = open_file(TEXT_RES_FILE_NE) 
+write_named_entities_file(NE_NLTK_FILE, text_NE.split("\n"), "NLTK")
+write_named_entities_conll2003_file(NE_CONLL_NLTK_FILE, text_NE.split("\n"), "NLTK")
+
